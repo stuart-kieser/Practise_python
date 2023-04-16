cfd2e2029795c32c4eba3266b3ad2d9301f7aee7
io <stuart@kieser.co.za>
2023-04-16 14:45:54 +0200

    Lowest_common_ancestor_of_a_BST_235.py
    I struggled with this one and still do, the  BST is strange to me and ill have to go over it again a couple times, i did use a solution because i was struggling to understand it

---
+++ Lowest_common_ancestor_of_a_BST_235.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+# p and q val inserted into x
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, q, p)
+        else:
+            return root
