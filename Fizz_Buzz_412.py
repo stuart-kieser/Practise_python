1f7538b663f815e825a62578b3d272b41d7dd093
io <stuart@kieser.co.za>
2023-04-17 13:23:00 +0200

    Fizz_Buzz_412.py
    Fizz_buzz was easy, the program is simple to understand too. Only the first if statement was challenging but splitting the criteria simplified it

---
+++ Fizz_Buzz_412.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        rtype = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                rtype.append("FizzBuzz")
+            elif i % 3 == 0:
+                rtype.append("Fizz")
+            elif i % 5 == 0:
+                rtype.append("Buzz")
+            else:
+                rtype.append(str(i))
+
+        return print(rtype)
+
+
+Solution.fizzBuzz(None, 3)
+Solution.fizzBuzz(None, 5)
+Solution.fizzBuzz(None, 15)  # 15 only returns fizz and not fizzbuzz as it should
